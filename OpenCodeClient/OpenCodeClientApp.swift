9fb80862a1d9bb5c2b12e1e1d613dbae0855d040
Mauricio Istúriz <isturizmauricio@gmail.com>
2026-07-19 20:52:56 -0400

    Initial Commit

---
+++ OpenCodeClient/OpenCodeClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenCodeClientApp.swift
+//  OpenCodeClient
+//
+//  Created by Mauricio Istúriz on 7/19/26.
+//
+
+import SwiftUI
+
+@main
+struct OpenCodeClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
